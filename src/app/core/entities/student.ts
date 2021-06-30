8f2082a1fbcffe574a53b158ab264876cdc05690
carloshximenes <carlos.ximenes@saude.ce.gov.br>
2021-06-30 13:20:27 -0300

    Criação das classes referentes aos objetos no json

---
+++ src/app/core/entities/student.ts
@@ -0,0 +1,10 @@
+import { Classe } from "./classe";
+import { Degree } from "./degree";
+
+export class Students {
+  id: number;
+  ra: number;
+  name: string;
+  degree: Degree;
+  class: Classe;
+}
